18771ab42778a71bd0f63b7ff40fb7637256c4e4
Christopher J. Walker <cjwalk92@gmail.com>
2021-09-07 07:34:27 -0400

    Update KivaCommandTester.java

--- KivaWorld/KivaCommandTester.java
+++ KivaWorld/KivaCommandTester.java
@@ -4,6 +4,26 @@ public class KivaCommandTester {
         System.out.println(command);
         System.out.println(command.getDirectionKey());
     }
-
+    public void testTurnLeft() {
+        KivaCommand command = KivaCommand.TURN_LEFT;
+        System.out.println(command);
+        System.out.println(command.getDirectionKey());
+    }
+    public void testTurnRIGHT() {
+        KivaCommand command = KivaCommand.TURN_RIGHT;
+        System.out.println(command);
+        System.out.println(command.getDirectionKey());
+    }
+    public void testTake() {
+        KivaCommand command = KivaCommand.TAKE;
+        System.out.println(command);
+        System.out.println(command.getDirectionKey());
+    }
+    public void testDrop() {
+        KivaCommand command = KivaCommand.DROP;
+        System.out.println(command);
+        System.out.println(command.getDirectionKey());
+    }
+    
     // For you: create four more methods, each testing a different command.
 }
