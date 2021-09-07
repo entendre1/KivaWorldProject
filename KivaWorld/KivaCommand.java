f2511533504a4d2bacae56b3aefa5433beec3391
Christopher J. Walker <cjwalk92@gmail.com>
2021-09-06 22:15:33 -0400

    Add files via upload

---
+++ KivaWorld/KivaCommand.java
@@ -0,0 +1,23 @@
+
+/**
+ * Write a description of KivaCommand here.
+ * 
+ * @author (Christopher J. Walker) 
+ * @version (Version 1.0)
+ */
+public enum KivaCommand {
+FORWARD ('F'),
+TURN_LEFT ('L'),
+TURN_RIGHT ('R'),
+TAKE ('T'),
+DROP ('D');
+
+KivaCommand(char directionKey) {
+    
+}
+public char getDirectionKey() {
+
+return 'a';
+}
+
+}
