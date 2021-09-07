0bf53712f02feddef524dbdabe2ec7f23a2138d1
Christopher J. Walker <cjwalk92@gmail.com>
2021-09-07 03:59:03 -0400

    Update KivaCommand.java

--- KivaWorld/KivaCommand.java
+++ KivaWorld/KivaCommand.java
@@ -6,14 +6,16 @@
  * @version (Version 1.0)
  */
 public enum KivaCommand {
-FORWARD ('F'),
-TURN_LEFT ('L'),
-TURN_RIGHT ('R'),
-TAKE ('T'),
-DROP ('D');
-
-KivaCommand(char directionKey) {
+FORWARD('F'),
+TURN_LEFT('L'),
+TURN_RIGHT('R'),
+TAKE('T'),
+DROP('D');
     
+private char directionKey;
+    
+private KivaCommand(char key) {
+ directionKey = key;   
 }
 public char getDirectionKey() {
 
